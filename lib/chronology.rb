a8e506f4e49f3d4b38f928d6abbe316b8db27a92
Paul Stefan Ort <mail@paulstefanort.com>
2014-02-26 21:48:07 -0600

    Add date-related operations.

--- lib/chronology.rb
+++ lib/chronology.rb
@@ -1,9 +1,37 @@
+require 'date'
+
 class Chronology
   def self.days_since(date)
     DateTime.now.mjd - date.mjd
   end
 
+  def self.weeks_since(date)
+    self.days_since(date) / 7
+  end
+
+  def self.months_since(date)
+    now = DateTime.now
+    (now.month - date.month) + 12 * (now.year - date.year)
+  end
+
   def self.years_since(date)
     (self.days_since(date) / 365.25).round(2)
+  end
+
+  def self.days_until(date)
+    date.mjd - DateTime.now.mjd
+  end
+
+  def self.weeks_until(date)
+    self.days_until(date) / 7
+  end
+
+  def self.months_until(date)
+    now = DateTime.now
+    (date.month - now.month) + 12 * (date.year - now.year)
+  end
+
+  def self.years_until(date)
+    (self.days_until(date) / 365.25).round(2)
   end
 end
